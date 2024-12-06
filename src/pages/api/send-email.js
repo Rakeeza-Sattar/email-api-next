3fdd89dd3f94f1c68dbf67cb131edb74afeefe53
Rakeeza-Sattar <rakeezasattar53@gmail.com>
2024-12-06 20:28:52 +0500

    deployment

--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -32,6 +32,8 @@ export default async function handler(req, res) {
   ) {
     return res.status(400).json({ error: "All fields are required." });
   }
+  console.log("EMAIL:", process.env.EMAIL);
+  console.log("PASSWORD:", process.env.PASSWORD);
 
   // Nodemailer transporter
   const transporter = nodemailer.createTransport({
